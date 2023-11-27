a680403a35b0a7ca7e67aa8eada00759cbf1231d
JitendraKumar1092 <jitendra.kumar_cs20@gla.ac.in>
2023-11-27 14:34:18 +0530

    pushing server

--- src/index.js
+++ src/index.js
@@ -7,20 +7,31 @@ import cors from "cors";
 import connectDB from "./mongodb/connect.js";
 dotenv.config();
 const app = express();
+
+const url = process.env.MONGO_URL;
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+
 app.use("/api/v1/post", postRoutes);
+
 app.use("/api/v1/imaginary", imaginaryRoutes);
+
 app.get("/", async (req, res) => {
   res.send("hello from Imaginary");
 });
-const url = process.env.MONGO_URL;
-const startServer = async () => {
-  try {
-    connectDB(url);
-    app.listen(8080, () => console.log("server has started on port 8080"));
-  } catch (err) {
-    console.log(err);
-  }
-};
-startServer();
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+connectDB(url);
+
+// const startServer = async () => {
+//   try {
+//     app.listen(3000, () => console.log("server has started on port 8080"));
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+// startServer();
+
+export default app;
