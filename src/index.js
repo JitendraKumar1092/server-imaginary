3689e3de551c4daadbf417be60d5ae264e96407f
Jitendra Kumar <jitendra.kumar_cs20@gla.ac.in>
2023-12-13 02:23:43 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -25,7 +25,11 @@ app.use(express.json({ limit: "50mb" }));
 
 app.use("/api/v1/post", postRoutes);
 
-app.use("/api/v1/imaginary", imaginaryRoutes);
+app.use("/api/v1/imaginary", (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://imaginary-ai.vercel.app");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+}, imaginaryRoutes);
 
 app.get("/", async (req, res) => {
   res.send("hello from Imaginary");
